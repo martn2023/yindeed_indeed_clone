a638b24f8c297c8a91ef8747d7c64c6870a237ab
Martin Lo <mdangerlo@gmail.com>
2024-03-25 14:43:22 +0700

    fixed size of textbox for company's long description in admin panel

--- job_catalog/admin.py
+++ job_catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import EmployerOrganization
 
-# Register your models here.
+class EmployerOrganizationAdmin(admin.ModelAdmin):
+    list_display = ('employer_org_name','employer_org_description_box')  #need to be what's in the models currently, not what you want the naked eye to see
+    search_fields = ['employer_org_name']
+
+admin.site.register(EmployerOrganization, EmployerOrganizationAdmin)  #need this to tie whats in the models (1st arg) to what shows up in admin panel (2nd arg)
--- job_catalog/models.py
+++ job_catalog/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class EmployerOrganization(models.Model):
     employer_org_name = models.CharField(max_length = 50)
-    employer_org_description_box = models.CharField(
-        max_length = 1000,
+    employer_org_description_box = models.TextField(
         default = 'Describe your organization, but not a specific role or requirements',
         blank = False  #we do not allow job posters to create employer orgs without a description
         )
+
+    def __str__(self):
+        return self.employer_org_name
