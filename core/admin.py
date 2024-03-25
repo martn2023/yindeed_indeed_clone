cf935844a0269545c6e61255cac024019b613d9d
Martin Lo <mdangerlo@gmail.com>
2024-03-25 19:36:06 +0700

    admin panel list of users now shows groups

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_groups')
+
+    def get_groups(self, obj):
+        return ", ".join([group.name for group in obj.groups.all()])
+    get_groups.short_description = 'Groups'
+
+# Re-register UserAdmin
+admin.site.unregister(User)  #we are replacing standard view of users in the admin panel
+admin.site.register(User, UserAdmin)
