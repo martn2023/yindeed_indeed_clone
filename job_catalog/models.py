f5f9700a7fdd5cc3a57ea819520e5580eb32874a
Martin Lo <mdangerlo@gmail.com>
2024-03-25 14:08:46 +0700

    first model EMPLOYER_ORG added to models, but not declared in admin dashboard

--- job_catalog/models.py
+++ job_catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
 # Create your models here.
+
+class EmployerOrganization(models.Model):
+    employer_org_name = models.CharField(max_length = 50)
+    employer_org_description_box = models.CharField(
+        max_length = 1000,
+        default = 'Describe your organization, but not a specific role or requirements',
+        blank = False  #we do not allow job posters to create employer orgs without a description
+        )
