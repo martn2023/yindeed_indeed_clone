4b465f0b8dc8c7902c0e70176c935267dd24d5a2
Martin Lo <mdangerlo@gmail.com>
2024-03-26 16:20:59 +0700

    root.com/register_user works!

--- core/views.py
+++ core/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import render
 def home_view(request):
     return render(request, 'home.html')
 
-class RegisterNewUserView(CreateView):   #view for account registration
+class RegisterUserView(CreateView):   #view for account registration
     form_class = UserCreationForm
     template_name = 'core/user_registration.html'  # remember that we have a master templates folder at root level
     success_url = reverse_lazy('core:login')  # attempting to redirect users to login page after successful registration, added /core directory above
