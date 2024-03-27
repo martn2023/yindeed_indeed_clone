548de74ec808716e3903d2d8cfac4585c2704174
Martin Lo <mdangerlo@gmail.com>
2024-03-27 21:52:39 +0700

    Jobs modeled and placed in admin, but unlinked to orgs

--- job_catalog/admin.py
+++ job_catalog/admin.py
@@ -3,10 +3,13 @@
 
 
 from django.contrib import admin
-from .models import EmployerOrganization
+from .models import EmployerOrganization, JobPosting
 
 class EmployerOrganizationAdmin(admin.ModelAdmin):
     list_display = ('employer_org_name','employer_org_description_box')  #need to be what's in the models currently, not what you want the naked eye to see
     search_fields = ['employer_org_name']
 
 admin.site.register(EmployerOrganization, EmployerOrganizationAdmin)  #need this to tie whats in the models (1st arg) to what shows up in admin panel (2nd arg)
+
+
+admin.site.register(JobPosting)
--- job_catalog/models.py
+++ job_catalog/models.py
@@ -2,6 +2,7 @@
 
 
 from django.db import models
+from django.utils import timezone  #need this or we can't auto-set dates of job posting originations
 # Create your models here.
 
 class EmployerOrganization(models.Model):
@@ -10,6 +11,21 @@ class EmployerOrganization(models.Model):
         default = 'Describe your organization, but not a specific role or requirements',
         blank = False  #we do not allow job posters to create employer orgs without a description
         )
-
     def __str__(self):
         return self.employer_org_name
+
+
+class JobPosting(models.Model):
+    title = models.CharField(max_length=100)
+    role_overview = models.TextField()
+    role_requirements_and_preferences = models.TextField()
+    action_steps = models.TextField()
+    post_date = models.DateTimeField(auto_now=True)
+    expiration_date = models.DateTimeField(default=timezone.now() + timezone.timedelta(days=90))
+    is_active = models.BooleanField(default=True)
+
+    # in django, save function is always called when you make a new job. we just overwrote it to make it more expansive in responsibility
+    def save(self, *args, **kwargs):  #technical debt, as this check is only going to run when a fresh job is created
+        if self.expiration_date < timezone.now():
+            self.is_active = False
+        super().save(*args, **kwargs)
