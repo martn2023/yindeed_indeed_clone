75dd9798bac602db736666753d8bdb19d8ef81b8
Martin Lo <mdangerlo@gmail.com>
2024-03-27 14:19:29 +0700

    useless login page replaced with auth path that asks for username + pw

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RegisterUserView, home_view  # Ensure your views are imported
+from .views import RegisterUserView, LoginStartView, home_view  # Ensure your views are imported
 
 app_name = 'core'  # take note that the app was called out specifically to avoid conflicts during url such as core:details vs jobs:details
 
 urlpatterns = [
     path('', home_view, name='home'),  # Existing homepage URL
-    path('register_user/', RegisterUserView.as_view(), name='register'),  # URL for the registration view
+    path('register_user/', RegisterUserView.as_view(), name = 'register'),  # URL for the registration view
+    path('login_start/', LoginStartView.as_view(), name = 'login_start')
     # Add other URLs as needed
 ]
--- core/views.py
+++ core/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 # core/views.py
 from django.contrib.auth.forms import UserCreationForm  #premade view coming out of auth app?
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy  #url isn't formed until a trigger event (such as a successful registration), which means harder to test
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView  #need to pull in 2 django pre-built forms
 from django.shortcuts import render
+
+
 
 def home_view(request):
     return render(request, 'home.html')
@@ -14,3 +17,6 @@ class RegisterUserView(CreateView):   #view for account registration
     form_class = UserCreationForm
     template_name = 'core/user_registration.html'  # remember that we have a master templates folder at root level
     success_url = reverse_lazy('core:login')  # attempting to redirect users to login page after successful registration, added /core directory above
+
+class LoginStartView(LoginView):   #view for attempting to authenticate
+    template_name = 'core/user_login.html'  # remember that we have a master templates folder at root level
