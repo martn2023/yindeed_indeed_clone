8a218fa89d2ee2521d39d233d9d77aae8bdc2a68
Martin Lo <mdangerlo@gmail.com>
2024-03-29 21:41:47 +0700

    users are now foreign keyed to orgs

--- core/models.py
+++ core/models.py
@@ -1,12 +1,26 @@
-from django.db import models  #This line is only needed if you're defining models in this file.
-
+from django.conf import settings
+from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User, Group
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    organization = models.ForeignKey('job_catalog.EmployerOrganization', on_delete=models.SET_NULL, null=True, blank=True, related_name='user_profiles')
+    # Include other fields for your UserProfile model here
+
+# Function to handle adding new users to the JobSeekers group
 def auto_add_to_job_seekers_group(sender, instance, created, **kwargs):
     if created:
         job_seekers_group, _ = Group.objects.get_or_create(name='JobSeekers')
         instance.groups.add(job_seekers_group)
 
-# Connect the signal handling function to the post_save signal of the User model
+# Function to handle creating or updating a user profile
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        instance.profile.save()
+
+# Connect signals
 post_save.connect(auto_add_to_job_seekers_group, sender=User)
+post_save.connect(create_or_update_user_profile, sender=settings.AUTH_USER_MODEL)
--- job_catalog/views.py
+++ job_catalog/views.py
@@ -11,20 +11,20 @@ def index(request):
 def display_all_companies(request):  #not sure where "request" comes from
     all_organizations = EmployerOrganization.objects.all()
     context = {'organizations_list': all_organizations}  # we are declaring, not pulling variables here
-    return render(request, 'jobs_catalog/all_companies.html' ,context)  #the 2nd argument auto-requested a template_name, and it will default to the templates folder as the root
+    return render(request, 'job_catalog/all_companies.html' ,context)  #the 2nd argument auto-requested a template_name, and it will default to the templates folder as the root
 
 
 
 def display_all_jobs(request):
     all_jobs = JobPosting.objects.all()
     context = {'job_postings_list': all_jobs}  # we are declaring, not pulling variables here
-    return render(request, 'jobs_catalog/all_jobs.html' ,context)
+    return render(request, 'job_catalog/all_jobs.html' ,context)
 
 
 def job_details(request, job_id):
     job_instance = get_object_or_404(JobPosting, pk = job_id)  #where is job_id coming from? django built in language? i think it's drawing from the URL
     context = {'job_instance': job_instance}  # we are declaring, not pulling variables here
-    return render(request, 'jobs_catalog/job_details.html' ,context)
+    return render(request, 'job_catalog/job_details.html' ,context)
 
 def company_details(request, company_id):
     company_instance = get_object_or_404(EmployerOrganization, pk=company_id)
@@ -38,4 +38,4 @@ def company_details(request, company_id):
         'total_job_postings': total_job_postings,  # Total number of jobs posted
         'active_job_postings': active_job_postings,  # Number of active jobs
     }
-    return render(request, 'jobs_catalog/company_details.html', context)
+    return render(request, 'job_catalog/company_details.html', context)
