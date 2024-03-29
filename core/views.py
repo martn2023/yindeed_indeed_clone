269a4ce010f1156aad2df7d42a9cbd9d6cce123e
Martin Lo <mdangerlo@gmail.com>
2024-03-29 16:51:27 +0700

    users can now claim orgs

--- core/views.py
+++ core/views.py
@@ -1,36 +1,28 @@
-from django.shortcuts import render
-
-# Create your views here.
-# core/views.py
-
+from django.shortcuts import render, redirect
 from django.contrib import messages
-
-from django.contrib.auth.forms import UserCreationForm  #premade view coming out of auth app?
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.models import Group
-
-from django.urls import reverse_lazy  #url isn't formed until a trigger event (such as a successful registration), which means harder to test
-from django.urls import reverse
-
 from django.views import View
-from django.views.generic.edit import CreateView  #need to pull in 2 django pre-built forms
-
-from django.shortcuts import render
-from django.shortcuts import redirect
-
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from job_catalog.models import EmployerOrganization
+from django.core.exceptions import ValidationError
 
 
 def home_view(request):
     return render(request, 'home.html')
 
-class RegisterUserView(CreateView):   #view for account registration
-    form_class = UserCreationForm
-    template_name = 'core/user_registration.html'  # remember that we have a master templates folder at root level
-    success_url = reverse_lazy('core:login_start')  # attempting to redirect users to login page after successful registration, added /core directory above
 
-class LoginStartView(LoginView):   #view for attempting to authenticate
-    template_name = 'core/user_login.html'  # remember that we have a master templates folder at root level
+class RegisterUserView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'core/user_registration.html'
+    success_url = reverse_lazy('core:login_start')
+
+
+class LoginStartView(LoginView):
+    template_name = 'core/user_login.html'
+
 
 class ClaimOrganizationView(View):
     template_name = 'core/representative_claims_company.html'
@@ -40,14 +32,24 @@ class ClaimOrganizationView(View):
         return render(request, self.template_name, {'organizations': organizations})
 
     def post(self, request, *args, **kwargs):
-        org_id = request.POST.get('organization')
-        claim_token = request.POST.get('claim_token')
-        try:
-            organization = EmployerOrganization.objects.get(id=org_id, representative_claim_token=claim_token)
-            job_poster_group, _ = Group.objects.get_or_create(name='JobPosters')
-            request.user.groups.add(job_poster_group)
-            # Redirect to the home page:
-            return redirect(reverse('core:home_view'))  # Use the name of your home page URL
-        except EmployerOrganization.DoesNotExist:
-            messages.error(request, "Invalid organization or claim token.")
-            return render(request, self.template_name)
+        if request.user.is_authenticated:
+            org_id = request.POST.get('organization')
+            claim_token = request.POST.get('claim_token')
+            try:
+                organization = EmployerOrganization.objects.get(id=org_id)
+                if organization.representative_claim_token != claim_token:
+                    messages.error(request, "CLAIM REJECTED: Incorrect password.")
+                    organizations = EmployerOrganization.objects.all()
+                    return render(request, self.template_name, {'organizations': organizations})
+
+                job_poster_group, _ = Group.objects.get_or_create(name='JobPosters')
+                request.user.groups.add(job_poster_group)
+
+                return redirect('core:home')
+            except EmployerOrganization.DoesNotExist:
+                messages.error(request, "CLAIM REJECTED: Organization not found.")
+        else:
+            messages.warning(request, "You must be logged in to claim an organization.")
+
+        organizations = EmployerOrganization.objects.all()
+        return render(request, self.template_name, {'organizations': organizations})
