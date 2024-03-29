0bd06d5db235546124441ddf2efc828e127e1425
Martin Lo <mdangerlo@gmail.com>
2024-03-29 11:43:28 +0700

    new accts auto-add to SEEKERS group

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db import models  #This line is only needed if you're defining models in this file.
 
-# Create your models here.
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User, Group
+
+def auto_add_to_job_seekers_group(sender, instance, created, **kwargs):
+    if created:
+        job_seekers_group, _ = Group.objects.get_or_create(name='Job Seekers')
+        instance.groups.add(job_seekers_group)
+
+# Connect the signal handling function to the post_save signal of the User model
+post_save.connect(auto_add_to_job_seekers_group, sender=User)
