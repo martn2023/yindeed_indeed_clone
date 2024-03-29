43a95d51dad076b80aff7c1a08e9045857131cdb
Martin Lo <mdangerlo@gmail.com>
2024-03-29 13:01:18 +0700

    org claim url, view, template

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import home_view, RegisterUserView, LoginStartView  # Ensure your views are imported
 from django.contrib.auth.views import LogoutView
+from .views import ClaimOrganizationView
 
 
 app_name = 'core'  # take note that the app was called out specifically to avoid conflicts during url such as core:details vs jobs:details
@@ -10,4 +11,6 @@ urlpatterns = [
     path('register_user/', RegisterUserView.as_view(), name = 'register'),  # URL for the registration view
     path('login_start/', LoginStartView.as_view(), name = 'login_start'),
     path('logout/', LogoutView.as_view(), name = 'logout'), #there is no logout page, we send back to home, but this code might not work, we might have to override in settings.py
+
+    path('claim_org/', ClaimOrganizationView.as_view(), name='claim_organization'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 # core/views.py
+
+from django.contrib import messages
+
 from django.contrib.auth.forms import UserCreationForm  #premade view coming out of auth app?
 from django.contrib.auth.views import LoginView
-from django.urls import reverse_lazy  #url isn't formed until a trigger event (such as a successful registration), which means harder to test
-from django.views.generic.edit import CreateView  #need to pull in 2 django pre-built forms
-from django.shortcuts import render
+from django.contrib.auth.models import Group
 
+from django.urls import reverse_lazy  #url isn't formed until a trigger event (such as a successful registration), which means harder to test
+from django.urls import reverse
+
+from django.views import View
+from django.views.generic.edit import CreateView  #need to pull in 2 django pre-built forms
+
+from django.shortcuts import render
+from django.shortcuts import redirect
+
+from job_catalog.models import EmployerOrganization
 
 
 def home_view(request):
@@ -21,3 +32,22 @@ class RegisterUserView(CreateView):   #view for account registration
 class LoginStartView(LoginView):   #view for attempting to authenticate
     template_name = 'core/user_login.html'  # remember that we have a master templates folder at root level
 
+class ClaimOrganizationView(View):
+    template_name = 'core/representative_claims_company.html'
+
+    def get(self, request, *args, **kwargs):
+        organizations = EmployerOrganization.objects.all()
+        return render(request, self.template_name, {'organizations': organizations})
+
+    def post(self, request, *args, **kwargs):
+        org_id = request.POST.get('organization')
+        claim_token = request.POST.get('claim_token')
+        try:
+            organization = EmployerOrganization.objects.get(id=org_id, representative_claim_token=claim_token)
+            job_poster_group, _ = Group.objects.get_or_create(name='JobPosters')
+            request.user.groups.add(job_poster_group)
+            # Redirect to the home page:
+            return redirect(reverse('core:home_view'))  # Use the name of your home page URL
+        except EmployerOrganization.DoesNotExist:
+            messages.error(request, "Invalid organization or claim token.")
+            return render(request, self.template_name)
