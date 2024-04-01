dc1ef24d831e4750501fe3b5bf9d65f361910d02
Martin Lo <mdangerlo@gmail.com>
2024-04-01 15:57:16 +0700

    foundation for searching job apps set in admin, cant test until a job app exists

--- job_applications/admin.py
+++ job_applications/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from .models import JobApplication
 
-
 class JobApplicationsAdmin(admin.ModelAdmin):
-    list_display = ('id','job_posting', 'user', 'linkedin_url', 'submit_date')
-    #list_filter = ('organization', 'post_date', 'is_active')
-    search_fields = ('user',)  # Assuming organization has a name field
+    list_display = ('id', 'job_posting', 'user', 'linkedin_url', 'submit_date')
+    search_fields = ('user__username', 'job_posting__organization__employer_org_name', 'job_posting__title')
 
 admin.site.register(JobApplication, JobApplicationsAdmin)
