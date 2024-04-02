02bbb687cd4dc2a66b949955ce4ddc4324dbce4b
Martin Lo <mdangerlo@gmail.com>
2024-04-02 15:42:43 +0700

    admin panel details job postings

--- job_applications/admin.py
+++ job_applications/admin.py
@@ -1,18 +1,45 @@
 from django.contrib import admin
 from .models import JobApplication
 
-
 class JobApplicationsAdmin(admin.ModelAdmin):
+
+    def get_job_posting_title(self, obj):
+        # This method will return the title of the job posting.
+        return obj.job_posting.title
+    get_job_posting_title.short_description = 'Job Title'
 
     def get_employer_organization_name(self, obj):
         # This method will return the employer organization name related to the job posting.
         return obj.job_posting.organization.employer_org_name
+    get_employer_organization_name.short_description = 'Employer Organization'
 
-    get_employer_organization_name.short_description = 'Employer Organization'  # Sets column name
+    def get_job_posting_id(self, obj):
+        # This method will return the ID of the job posting.
+        return obj.job_posting.id
+    get_job_posting_id.short_description = 'Job Posting ID'
 
-    list_display = ('id', 'get_employer_organization_name', 'user', 'linkedin_url', 'submit_date')
+    def get_employer_organization_id(self, obj):
+        # This method will return the ID of the employer organization related to the job posting.
+        return obj.job_posting.organization.id
+    get_employer_organization_id.short_description = 'Employer Org ID'
 
-    search_fields = ('user__username', 'job_posting__organization__employer_org_name', 'job_posting__title')
+    list_display = (
+        'id',
 
+        'get_employer_organization_name',
+        'get_employer_organization_id',
+
+        'get_job_posting_title',
+        'get_job_posting_id',
+
+        'user',
+        'submit_date',
+        'linkedin_url'
+    )
+    search_fields = (
+        'user__username',
+        'job_posting__title',
+        'job_posting__organization__employer_org_name'
+    )
 
 admin.site.register(JobApplication, JobApplicationsAdmin)
