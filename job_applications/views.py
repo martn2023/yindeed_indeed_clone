cc3f8fa8ba608b93a9202554fbdbc2e9e1fa19b6
Martin Lo <mdangerlo@gmail.com>
2024-04-03 11:08:48 +0700

    company-wide applicant sums

--- job_applications/views.py
+++ job_applications/views.py
@@ -4,32 +4,25 @@ from django.contrib import messages
 from collections import defaultdict
 from .forms import JobApplicationForm
 from .models import JobPosting, JobApplication
-from core.models import UserProfile
-
 
 def apply_for_job(request, job_posting_id):
     job_posting = get_object_or_404(JobPosting, pk=job_posting_id)
     if request.method == 'POST':
         form = JobApplicationForm(request.POST, request.FILES)
         if form.is_valid():
-            # Check for an existing application
             existing_application = JobApplication.objects.filter(
                 user=request.user,
                 job_posting=job_posting
             ).exists()
-
             if existing_application:
-                # User has already applied
                 messages.error(request, "You have already applied for this job.")
                 return redirect('job_applications:apply_for_job', job_posting_id=job_posting_id)
-
             application = form.save(commit=False)
             application.user = request.user
             application.job_posting = job_posting
             application.save()
             return redirect('job_applications:application_accepted', application_id=application.id)
         else:
-            # Form is not valid, show the form again with errors
             context = {
                 'form': form,
                 'job_posting': job_posting
@@ -37,63 +30,46 @@ def apply_for_job(request, job_posting_id):
             return render(request, 'job_applications/apply_for_job.html', context)
     else:
         form = JobApplicationForm(initial={'job_posting': job_posting_id, 'user': request.user.id})
-
-    context = {
-        'form': form,
-        'job_posting': job_posting
-    }
-    return render(request, 'job_applications/apply_for_job.html', context)
-
+        context = {
+            'form': form,
+            'job_posting': job_posting
+        }
+        return render(request, 'job_applications/apply_for_job.html', context)
 
 def application_accepted(request, application_id):
     application = get_object_or_404(JobApplication, pk=application_id)
     return render(request, 'job_applications/application_accepted.html', {'application': application})
 
-
 def my_job_applications(request):
     if not request.user.is_authenticated:
-        # Redirect to login page
         return redirect(reverse('core:login_start'))
-
     job_applications = JobApplication.objects.filter(user=request.user).select_related(
         'job_posting__organization').order_by('-submit_date')
     applications_grouped = defaultdict(list)
     for job_app in job_applications:
         applications_grouped[job_app.job_posting.organization].append(job_app)
-
     context = {
         'applications_grouped': dict(applications_grouped)
     }
     return render(request, 'job_applications/users_job_applications.html', context)
 
-
 def view_applications_for_employer(request):
     if not request.user.is_authenticated:
-        return redirect('core:login_start')  # Redirect unauthenticated users to login page
-
-    print("User:", request.user.username)
-    print("Is Superuser:", request.user.is_superuser)
-    print("Has UserProfile:", hasattr(request.user, 'userprofile'))
-    print("Organization:", getattr(request.user.profile, 'organization', 'No organization'))
-
-
-
-    # Assuming the User model has a one-to-one link to a UserProfile model, which in turn is linked to an EmployerOrganization.
+        return redirect('core:login_start')
     if hasattr(request.user, 'profile') and request.user.profile.organization:
         employer_org = request.user.profile.organization
         job_postings = JobPosting.objects.filter(organization=employer_org)
-        job_applications = JobApplication.objects.filter(job_posting__in=job_postings).select_related('user').order_by('-submit_date')
-
+        total_applications_count = 0
         applications_grouped_by_job = defaultdict(list)
-        for application in job_applications:
-            applications_grouped_by_job[application.job_posting].append(application)
-
+        for job_posting in job_postings:
+            applications = JobApplication.objects.filter(job_posting=job_posting).select_related('user').order_by('submit_date')
+            applications_grouped_by_job[job_posting].extend(applications)
+            total_applications_count += applications.count()
         context = {
             'applications_grouped_by_job': dict(applications_grouped_by_job),
-            'employer_org': employer_org,  # you can pass the organization to the template if needed
+            'employer_org': employer_org,
+            'total_applications_count': total_applications_count,
         }
         return render(request, 'job_applications/employer_applications.html', context)
     else:
-        # If the user is not linked to any organization or not an employer
-        # You can choose to redirect them or simply show an error page
-        return render(request, 'job_applications/unauthorized.html')  # Make sure to create this template or handle accordingly
+        return render(request, 'job_applications/unauthorized.html')
