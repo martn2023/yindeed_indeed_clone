0397c4609129887f6e576c7fab30ed9da5984ec3
Martin Lo <mdangerlo@gmail.com>
2024-04-03 19:57:58 +0700

    allowed hosts must include heroku

--- yindeed_proj_name/settings.py
+++ yindeed_proj_name/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-xfqfi^f0v-a_19)u)=_^=)p=1hg2k(sg!)v11kq2%8ch-weazo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['your_domain.com', 'yindeed-8a7537fcdc5b.herokuapp.com'] #or it wont load on internet
 
 # Application definition
 
